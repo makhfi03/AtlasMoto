6783091abae56b7bc1123d55ff9e169d1543090b
makhfi03 <ghita.makhfi.21@gmail.com>
2026-04-03 15:23:56 +0100

    Add MotoController with CRUD methods for managing motos

---
+++ app/Http/Controllers/MotoController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Moto;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class MotoController extends Controller
+{
+    public function index()
+    {
+        $motos = Moto::all();
+        return view('locations', compact('motos'));
+    }
+
+    public function home()
+    {
+        $motos = Moto::all();
+        return view('welcome', compact('motos'));
+    }
+
+    public function adminIndex()
+    {
+        $motos = Moto::all();
+        return view('admin.motos', compact('motos'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'image' => 'required|url',
+            'immatriculation' => 'required|unique:motos',
+            'kilometrage' => 'required|numeric',
+            'categorieMoto' => 'required',
+            'price_per_day' => 'required|numeric',
+        ]);
+
+        \App\Models\Moto::create([
+            'name' => $request->name,
+            'image' => $request->image,
+            'immatriculation' => $request->immatriculation,
+            'kilometrage' => $request->kilometrage,
+            'categorieMoto' => $request->categorieMoto,
+            'description' => $request->description,
+            'price_per_day' => $request->price_per_day,
+            'status' => 'disponible',
+        ]);
+
+        return redirect()->back()->with('success', 'Moto enregistrée avec succès !');
+    }
+
+    public function destroy(Moto $moto)
+    {
+        if ($moto->image) Storage::disk('public')->delete($moto->image);
+        $moto->delete();
+        return redirect()->back();
+    }
+}
