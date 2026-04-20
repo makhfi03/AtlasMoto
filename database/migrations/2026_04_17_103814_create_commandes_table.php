471e4f1124400c9588ff097abc2e5fd841ecf569
makhfi03 <ghita.makhfi.21@gmail.com>
2026-04-20 05:25:06 +0100

    new migration commandes

---
+++ database/migrations/2026_04_17_103814_create_commandes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->json('items');
+            $table->decimal('total', 10, 2);
+            $table->string('statut')->default('payé');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commandes');
+    }
+};
