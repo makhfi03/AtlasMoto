d54f66221774ffcdca3a1bc5b1e2679807e925f6
makhfi03 <ghita.makhfi.21@gmail.com>
2026-04-20 05:32:16 +0100

    add function mesCommandes

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -51,5 +50,12 @@ class User extends Authenticatable
     public function locations()
     {
         return $this->hasMany(Location::class);
+    }
+    public function mesCommandes()
+    {
+        $locations = Location::where('user_id', auth()->id())->with('moto')->latest()->get();
+        $achats = Commande::where('user_id', auth()->id())->latest()->get();
+
+        return view('user.commandes', compact('locations', 'achats'));
     }
 }
